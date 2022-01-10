4b4f4718549e58f8f7f696f8d700c709f3157874
mahdi05tazwar <83511272+mahdi05tazwar@users.noreply.github.com>
2022-01-10 18:35:39 +0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
-from setuptools import setup, find_packages
- 
-classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Developers',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
-]
- 
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-  name='minimal_encryption',
-  version='0.0.1',
-  description='A simple package/system encryption and decryption. The way of using the package has been described.',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='',  
-  author='Mahdi Tazwar Raeed',
-  author_email='mahdi05tazwar@gmail.com',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords=['encryption', 'encrypt', 'decrypt', 'decryption', 'encryptor'], 
-  packages=find_packages(),
-  install_requires=[''] 
+    name='mini_encryption',
+    version='0.0.1',
+    description="A simple encryption and decryption library. Usage has been described in this page; if not, then: Download the library and open a python file. Type \"from mini_encryption import explain\" and then in the next line, type \"explain()\"",
+    author='Mahdi Tazwar Raeed',
+    url = 'https://mahditaz.pythonanywhere.com',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['encryption', 'encrypt', 'encryptor', 'mini', 'simple', 'mahdi', 'tazwar', 'raeed', 'mahditaz', 'shaion'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.1',
+    py_modules=['mini_encryption'],
+    #package_dir={'':''},
+    install_requires = []
 )
